5911a9802e9f20336b80948c9a76a299fcc227fc
root <root@heidi.shack>
2013-10-16 20:05:13 +0200

    add something working

--- init.py
+++ init.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
-import json
+import json,sys
 from relaxxapi.relaxxapi import relaxx
 r = relaxx()
 current = r.get_current()
 #print(json.dumps(current))
+base_path="/home/shack/music"
+
+
+if not current:
+    print("Nothing is running, bailing out")
+    sys.exit(0)
+print(json.dumps( current))
 print("ssh shack@mpd.shack 'rm \"music/%s\"'" % current["file"])
+
+
+host = "mpd.shack"
+port = 22
+transport = paramiko.Transport((host, port))
+username = 'shack'
+passwd= 'shackit'
+transport.connect(username = username,password=passwd)
+sftp = paramiko.SFTPClient.from_transport(transport)
+print(sftp.stat('%s/%s'%(base_path,current["file"])))
+#print(sftp.unlink('%s/%s'%(base_path,current["file"])))
+sftp.close()
+transport.close()
