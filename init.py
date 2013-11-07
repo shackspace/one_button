47f9332c884ff85a69684d7f6b455763cbe4d381
makefu <github@syntax-fehler.de>
2013-11-08 00:08:55 +0100

    init: refactoring

--- init.py
+++ init.py
@@ -3,106 +3,65 @@ from threading import Timer
 import RPIO
 from RPIO import PWM
 import paramiko
-import json,sys
-from time import time
+import json
+import sys
+from time import time, sleep
 from relaxxapi.relaxxapi import relaxx
 r = None
-base_path="/home/shack/music"
-state=0
-# state 0 
-#       low :do nothing
-#       high:begin timing, goto state 1
-# 
-# timer sets state to 2 if state is still 1
-# 
-# state 1 
-#       low : reset timer, goto state 0
-#       high:do nothing (should not happen)
-# state 2:
-#       low : reset timer,
-# 
-# not current = off
-# current = on
-timer=0
-TIMEOUT=5
-#radio_list= [
+sftp_base_path = "/home/shack/music"
 
 
-button=17
-loud1=14
-loud2=15
-light=18
+button = 4
+loud1 = 18
+loud2 = 21
+light = 22
 
-def pwm_light():
-    
+state = 0
 
 
 def init_state():
     PWM.setup()
-    PWM.init_channel(0,1000)
-    PWM.add_channel_pulse(0,light,start=0,width=
-    state=0
+    PWM.init_channel(0, 1000)
+    state = 0
 
-def tell_gobbelz(name,author):
+
+def tell_gobbelz(name, author):
     import requests
-    import json
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    data = { 'text': '%s von %s wurde vernichtet!'%(name,author)}
-    #   curl -i -H "content-type: application/json" -X POST -d "{\"text\" : \"Hallo shackspace\"}" kiosk.shack:8080/say/
-    requests.post("http://kiosk.shack:8080/say/",data=json.dumps(data),headers=headers)
-    
-  
+    data = {'text': '%s von %s wurde vernichtet!' % (name, author)}
+    #  curl -i -H "content-type: application/json"
+    #     -X POST -d "{\"text\" : \"Hallo shackspace\"}" kiosk.shack:8080/say/
+    requests.post("http://kiosk.shack:8080/say/",
+                  data=json.dumps(data), headers=headers)
+ 
+
 def play_radio():
     #TODO play radio
-    if r.get_current().get("file","") == "http://ice.somafm.com/groovesalad": 
+    if r.get_current().get("file", "") == "http://ice.somafm.com/groovesalad":
         print("will not skip own sender")
         return
     print("playing radio")
     r.add_song("http://ice.somafm.com/groovesalad")
     r.play_last()
 
+
+def play_next():
+    try:
+        #sanity
+        if is_last_song():
+            raise Exception("Last song in playlist")
+        r.next_song()
+    except:
+        print("no next song, starting radio")
+        play_radio()
+
 def is_last_song():
     return r.get_current()["Pos"] == r.get_last()["Pos"]
 
-def rising_state(a,edge):
-    print("Button: %s , edge: %s"%(str(a),str(edge)))
-    return 
-    global state
-    global timer
-    global TIMEOUT
-    global r
-    if state == 0:
-        if edge:
-            timer=time()
-            print("Button pressed at %d"%timer)
-            state = 1
-    # less than 2 seconds
-    elif state == 1:
-        if not edge:
-            if ( time()-timer) < TIMEOUT :
-                print("timeout not reached (%d seconds of %d)\nplaying next song"% (time()-timer,TIMEOUT))
-                try:
-                    #sanity if the list is not empty, bail out
-                    if is_last_song(): raise Exception("Last song in playlist")
-                    # try to run next_song, this will always work...
-                    r.next_song()
 
-                except:
-                    print("no next song, starting radio")
-                    play_radio()
+def btn_transition(a,edge):
+    print("Button: %s , edge: %s" % (str(a), str(edge)))
 
-                    #print("no next song, clearing")
-
-                    #r.stop()
-                    #r.clear()
-                state = 0
-            else:
-	        state = 2
-    if state == 2:
-        print("Will Delete File (timer %d seconds)"% (time()-timer))
-        delete_current_music()
-        state = 0
-        
 
 def delete_current_music():
     current = r.get_current()
@@ -110,30 +69,28 @@ def delete_current_music():
     if not current:
         print("Nothing is running, bailing out")
         return
+    delete_remote_file(current)
+
+
+def delete_remote_file(current):
     try:
-        delete_remote_file(current["file"])
-        tell_gobbelz(current.get("Title","Unbekannter Title"),current.get("Artist","Unbekannter Kuenstler"))
+        sftp_delete_remote_file(current["file"])
+        tell_gobbelz(current.get("Title", "Unbekannter Title"),
+                     current.get("Artist", "Unbekannter Kuenstler"))
     except:
         print("Cannot delete remote file!")
 
-    try:
-        #sanity
-	if is_last_song(): raise Exception("Last song in playlist")
-        r.next_song()
-    except:
-	print("no next song, starting radio")
-        play_radio()
 
-def delete_remote_file(f):
+def sftp_delete_remote_file(f):
     host = "mpd.shack"
     port = 22
     transport = paramiko.Transport((host, port))
     username = 'shack'
-    passwd= 'shackit'
-    transport.connect(username = username,password=passwd)
+    passwd = 'shackit'
+    transport.connect(username=username, password=passwd)
     sftp = paramiko.SFTPClient.from_transport(transport)
     #print(sftp.stat('%s/%s'%(base_path,f)))
-    print(sftp.unlink('%s/%s'%(base_path,f)))
+    print(sftp.unlink('%s/%s' % (sftp_base_path, f)))
     sftp.close()
     transport.close()
 
