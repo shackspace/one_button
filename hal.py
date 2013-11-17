dace979a21f41fc08fb6c59882f564e7cc324048
root <root@soundflower.(none)>
2013-11-17 00:26:08 +0000

    fix hal timing

--- hal.py
+++ hal.py
@@ -5,7 +5,7 @@ PWM.setup()
 current = lower = 300
 up = True
 upper = 1600
-step = 50
+step = 30
 PWM.init_channel(0)
 hal_led = 17
 hal_speed=15
@@ -25,4 +25,4 @@ while 1:
         else:
             print("Fading up")
             up = not up
-    sleep(hal_speed)
+    sleep(1.0/hal_speed)
